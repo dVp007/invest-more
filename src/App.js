f89d7755c175de06d73b435afb2c83ed64664154
Dharmil Parekh <dharmilp@valuefy.com>
2022-03-08 09:26:20 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button title='Submit' onClick={()=>{alert("Hey I'm being clicked")}}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
